a2cbb39204e074a0b717ed8a3966ee91fc0f10b1
Edwyl <145359207+Edwyl@users.noreply.github.com>
2023-09-20 20:23:16 +0300

    Add files via upload

---
+++ FirstApplicationRodermel/Pages/PagePR2.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstApplicationRodermel.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PagePR2.xaml
+    /// </summary>
+    public partial class PagePR2 : Page
+    {
+        public PagePR2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, RoutedEventArgs e)
+        {
+            lstResult.Items.Clear();
+            double x = double.Parse(txtX.Text);
+            double p = double.Parse(txtY.Text);
+            double l = 0;
+            lstResult.Items.Add("Лаб.раб.№2. Выполнил Каламфуров Д.С.");
+            lstResult.Items.Add($"X={x}");
+            lstResult.Items.Add($"P={p}");
+            int n = 0;
+            if (rbtX2.IsChecked == true) n = 1;
+            else if (rbtExp.IsChecked == true) n = 2;
+            switch (n)
+            {
+                case 0:
+                    if (x > Math.Abs(p)) l = 2 * Math.Pow(Math.Sinh(x), 3) + 3 * Math.Pow(p, 2);
+                    else if (x == Math.Abs(p)) l = Math.Pow((Math.Sinh(x) - p), 2);
+                    else if (3 < x && x < Math.Abs(p)) l = Math.Abs(Math.Sinh(x) - p);
+                    else goto default;
+                    lstResult.Items.Add($"Результат l={Math.Round(l, 3)}");
+                    break;
+                case 1:
+                    if (x > Math.Abs(p)) l = 2 * Math.Pow(Math.Pow(x, 2), 3) + 3 * Math.Pow(p, 2);
+                    else if (x == Math.Abs(p)) l = Math.Pow((Math.Pow(x, 2) - p), 2);
+                    else if (3 < x && x < Math.Abs(p)) l = Math.Abs(Math.Pow(x, 2) - p);
+                    else goto default;
+                    lstResult.Items.Add($"Результат l={Math.Round(l, 3)}");
+                    break;
+                case 2:
+                    if (x > Math.Abs(p)) l = 2 * Math.Pow(Math.Exp(x), 3) + 3 * Math.Pow(p, 2);
+                    else if (x == Math.Abs(p)) l = Math.Pow((Math.Exp(x) - p), 2);
+                    else if (3 < x && x < Math.Abs(p)) l = Math.Abs(Math.Exp(x) - p);
+                    else goto default;
+                    lstResult.Items.Add($"Результат l={Math.Round(l, 3)}");
+                    break;
+                default:
+                    lstResult.Items.Add("Решение не найдено");
+                    break;
+            }
+        }
+            private void btnClear_Click(object sender, RoutedEventArgs e)
+            {
+                txtX.Clear();
+                txtY.Clear();
+                lstResult.Items.Clear();
+            }
+        }
+    }
---
+++ FirstApplicationRodermel/Pages/PagePR3.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstApplicationRodermel.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PagePR3.xaml
+    /// </summary>
+    public partial class PagePR3 : Page
+    {
+        public PagePR3()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, RoutedEventArgs e)
+        {
+            lstResult.Items.Clear();
+            double x0 = double.Parse(txtX0.Text);
+            double xk = double.Parse(txtXk.Text);
+            double dx = double.Parse(txtDx.Text);
+            double b = double.Parse(txtB.Text);
+            double y = 0;
+            lstResult.Items.Add("Лаб.раб.№3. Выполнил Каламфуров Д.C.");
+            lstResult.Items.Add($"Отрезок{x0}-{xk}");
+            lstResult.Items.Add($"шаг={dx}");
+            lstResult.Items.Add($"a={b}");
+            while (x0 <= xk)
+            {
+                y = 0.0025 * b * Math.Pow(x0,3) + Math.Sqrt(x0+Math.Exp(0.82));
+                lstResult.Items.Add($"x={x0}; y={y}");
+                x0 = x0 + dx;
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtX0.Clear();
+            txtXk.Clear();
+            txtDx.Clear();
+            txtB.Clear();
+            lstResult.Items.Clear();
+        }
+    }
+}
